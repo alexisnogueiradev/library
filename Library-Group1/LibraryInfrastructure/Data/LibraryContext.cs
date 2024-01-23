b58c1a22a9f9e5e60964a222aa42b74ac962ae3d
Alexis <alexis.nogueira.dev@gmail.com>
2024-01-23 10:59:46 +0100

    Initial Clean Architecture Created

---
+++ Library-Group1/LibraryInfrastructure/Data/LibraryContext.cs
@@ -0,0 +1,30 @@
+﻿using Library.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Domain> Domains { get; set; }
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().UseTpcMappingStrategy();
+            modelBuilder.Entity<Reader>().ToTable("Readers");
+            modelBuilder.Entity<Writer>().ToTable("Writers");
+            modelBuilder.Entity<Admin>().ToTable("Admins");
+        }
+    }
+}
