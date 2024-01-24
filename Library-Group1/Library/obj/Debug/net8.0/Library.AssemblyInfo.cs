d7988aa8735f120528bae44da4ed97e63f4ad11d
Alexis <alexis.nogueira.dev@gmail.com>
2024-01-24 10:58:00 +0100

    Fix GitIgnore v2

--- Library-Group1/Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library-Group1/Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6646e2f531c52ad692a515b166678bb87a935c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1438ba989c334ded3233bab2d1bb25f72cea2f98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
