5e33b4918faa5c92e9c98a47bb9f3fd8035a5cd2
FlorentM <f.madouri@free.fr>
2024-01-24 10:30:30 +0100

    AddAddresses

--- Library-Group1/Library/Program.cs
+++ Library-Group1/Library/Program.cs
@@ -11,6 +11,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDbContext<LibraryContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddScoped<IWriterRepository, WriterRepository>();
+builder.Services.AddScoped<IAddressRepository, AddressRepository>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
